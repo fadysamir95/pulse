ac5e13e79384c4c6e518c43641784f591b1d65a9
fadysamir95 <fadysamirdev@outllok.com>
2020-10-14 14:15:41 +0200

    updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-    
+    $(".mega-menu--nav li:first-child").mouseover(function() { 
+        $(".mega-menu--list").hide();
+        $(".mega-menu--list:first-child").show(500);
+    });
+    $(".mega-menu--nav li:nth-child(2)").mouseover(function() {
+        $(".mega-menu--list").hide();
+        $(".mega-menu--list:nth-child(2)").show(500);
+    });
+    $(".mega-menu--nav li:nth-child(3)").mouseover(function() {
+        $(".mega-menu--list").hide();
+        $(".mega-menu--list:nth-child(3)").show(500);
+    });
+    $(".mega-menu--nav li:nth-child(4)").mouseover(function() {
+        $(".mega-menu--list").hide();
+        $(".mega-menu--list:nth-child(4)").show(500);
+    });
+    $(".mega-menu--nav li:nth-child(5)").mouseover(function() {
+        $(".mega-menu--list").hide();
+        $(".mega-menu--list:nth-child(5)").show(500);
+    });
+    $(".mega-menu--nav li:nth-child(6)").mouseover(function() {
+        $(".mega-menu--list").hide();
+        $(".mega-menu--list:nth-child(6)").show(500);
+    });
 });
