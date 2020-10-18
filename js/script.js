fbecd8de9d152dc0fa37346a1f56fa6686b2df80
fadysamir95 <fadysamirdev@outllok.com>
2020-10-18 15:23:21 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,29 +1,70 @@
-$(document).ready(function(){
-    $(".mega-menu--nav li:first-child").mouseover(function() { 
-        $(".mega-menu--list").hide();
-        $(".mega-menu .mega-menu--nav li:first-child .mega-menu--list").show();
+$(document).ready(function () {
+    
+    //closing menu on click outside menu links
+    $(window).click(closeMenu);
+    $('.mega-menu--nav,#menu-toggle img').click(function(event){
+        event.stopPropagation();
     });
-    $(".mega-menu--nav li:nth-child(2)").mouseover(function() {
-        $(".mega-menu--list").hide();
-        $(".mega-menu .mega-menu--nav li:nth-child(2) .mega-menu--list").show();
+    $("#menu-toggle img").click(function () {
+        $(".mega-menu").toggle('show');
+        $("html").toggleClass('scroll');
+        $(".sidebar-logo").toggleClass('invisible');
+        if ($("#menu-toggle img").attr('src') == 'images/icons/menu.png')
+            $("#menu-toggle img").attr('src', 'images/icons/close-button.png');
+        else
+            $("#menu-toggle img").attr('src', 'images/icons/menu.png');
+
+        if ($('html').hasClass('scroll'))
+            menuInitialState();
     });
-    $(".mega-menu--nav li:nth-child(3)").mouseover(function() {
+    $(".mega-menu--nav li").mouseover(function () {
         $(".mega-menu--list").hide();
-        $(".mega-menu .mega-menu--nav li:nth-child(3) .mega-menu--list").show();
+        var menuID = $(this).attr('id');
+        $(".mega-menu .mega-menu--nav li#" + menuID + " .mega-menu--list").show();
     });
-    $(".mega-menu--nav li:nth-child(4)").mouseover(function() {
-        $(".mega-menu--list").hide();
-        $(".mega-menu .mega-menu--nav li:nth-child(4) .mega-menu--list").show();
-    });
-    $(".mega-menu--nav li:nth-child(5)").mouseover(function() {
-        $(".mega-menu--list").hide();
-        $(".mega-menu .mega-menu--nav li:nth-child(5) .mega-menu--list").show();
-    });
-    $(".mega-menu--nav li:nth-child(6)").mouseover(function() {
-        $(".mega-menu--list").hide();
-        $(".mega-menu .mega-menu--nav li:nth-child(6) .mega-menu--list").show();
-    });
-    $(".mega-menu--nav li").mouseout(function() {
-        $(".mega-menu--list").hide();
-    });
+
+    //map city selection
+    $('.offices li').click(function () {
+        $('.offices li').removeClass('selected');
+        $(this).addClass('selected');
+        var cityID = $(this).attr('for');
+        var region = $(this).attr('data-region');
+
+        $('.map-pointer').removeClass('selected');
+        $('.map-pointer .glowing').removeClass('glow-pointer');
+        $(".map-pointer[data-region='" + region + "']#" + cityID + "").addClass('selected');
+        $(".map-pointer[data-region='" + region + "']#" + cityID + " .glowing").addClass('glow-pointer');
+    })
+
+    // Search
+    $('.search-icon img').click(function () {
+        $(".search-input input").toggle('show');
+        if ($(".search-icon img").attr('src') == 'images/icons/search.png')
+            $(".search-icon img").attr('src', 'images/icons/close-search.png');
+        else
+            $(".search-icon img").attr('src', 'images/icons/search.png');
+    })
+    // $(document).click(function() {
+    //     $(".search-input input").hide();
+    //     if ($(".search-icon img").attr('src') == 'images/icons/search.png')
+    //         $(".search-icon img").attr('src', 'images/icons/close-search.png');
+    //     else
+    //         $(".search-icon img").attr('src', 'images/icons/search.png');
+    // });
+    // $(".search-input input").click(function(e) {
+    //     e.stopPropagation();
+    //     return false;
+    // });
 });
+
+function menuInitialState() {
+    $('.mega-menu .mega-menu--nav .item.item:first-child').removeClass('hovered')
+    $(".mega-menu .mega-menu--nav li .mega-menu--list").hide();
+}
+function closeMenu() {
+    $(".mega-menu").hide('slow');
+    $("html").removeClass('scroll');
+    $(".sidebar-logo").removeClass('invisible');
+    $("#menu-toggle img").attr('src', 'images/icons/menu.png');
+    menuInitialState();
+}
